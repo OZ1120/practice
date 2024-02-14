61163d3f28b533ae3ea537b15352146f131991e4
OZ <macoto8@naver.com>
2024-02-14 17:12:58 +0900

    240213

--- 04_Java/09_collection/src/edu/kh/collection/pack3/model/service/MapService.java
+++ 04_Java/09_collection/src/edu/kh/collection/pack3/model/service/MapService.java
@@ -1,6 +1,21 @@
 package edu.kh.collection.pack3.model.service;
 
+import java.util.HashMap;
+
 public class MapService {
 
+	public void method1() {
+		Map<Integer,String> map = new HashMap<Integer, String>();
+		
+
+		System.out.println(map.put(1, "에그마요"));
+		System.out.println(map.put(2, "로티세리바베큐"));
+		System.out.println(map.put(3, "스테이크앤치즈"));
+		System.out.println(map.put(3, "스파이시쉬림프")); // 3중복
+		
+		System.out.println(map);
+		
+	}
+	
 	
 }
